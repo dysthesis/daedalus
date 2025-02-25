880f30fafe39bdf9178985824775d774f5a1c1a2
Dysthesis <antheoraviel@protonmail.com>
2025-02-25 12:23:53 +1100

    Changes

--- config.nix
+++ config.nix
@@ -18,6 +18,10 @@
       ${readFile ./tmux.conf}
       ${readFile ./lackluster.tmux}
       bind -n C-f run-shell "tmux neww ${getExe sessioniser}"
+      bind -n M-a display-popup -E -w 75% -h 75% -b rounded "${getExe popup}"
+      bind -T popup M-a detach
+      # This lets us do scrollback and search within the popup
+      bind -T popup C-[ copy-mode
     '';
 
   sessioniser = import ./sessioniser.nix {
@@ -27,6 +31,8 @@
       "~/Documents/Projects"
     ];
   };
+  popup = import ./popup.nix {inherit pkgs;};
+
   plugins = with pkgs.tmuxPlugins; [
     vim-tmux-navigator
     sensible

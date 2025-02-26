e1af9bd193bb961bd23082bb49f4a10fea2889be
Dysthesis <antheoraviel@protonmail.com>
2025-02-26 12:13:52 +1100

    Typo

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -19,7 +19,7 @@ in rec {
         mkdir $out/libexec
         mv $out/bin/tmux $out/libexec/tmux-unwrapped
         makeWrapper $out/libexec/tmux-unwrapped $out/bin/tmux \
-        	--add-flags "-f ${config}"
+        	--add-flags "-f ${config}" \
           --set TERM "screen-256color"
       '';
   });

724a0ffe30b96d736884c7ccf3e5073fcb7eec93
Dysthesis <antheoraviel@protonmail.com>
2025-07-09 12:04:05 +1000

    Add deps to lazyjj popup

--- lazyjj-popup.nix
+++ lazyjj-popup.nix
@@ -2,37 +2,40 @@
   lib,
   pkgs,
   ...
-}:
-pkgs.writeShellScriptBin "git-popup.sh"
-/*
-sh
-*/
-''
-  session="_lazyjj_$(tmux display -p '#S')"
+}: let
+  deps = with pkgs; [git-crypt];
+in
+  pkgs.writeShellScriptBin "git-popup.sh"
+  /*
+  sh
+  */
+  ''
+    export PATH=${lib.makeBinPath deps}:$PATH
+    session="_lazyjj_$(tmux display -p '#S')"
 
-  detect_repo_type() {
-    local dir="$PWD"
-    while [ "$dir" != "/" ]; do
-      [ -d "$dir/.jj" ]  && { echo "jujutsu"; return 0; }
-      [ -d "$dir/.git" ] && { echo "git";     return 0; }
-      dir="$(dirname "$dir")"
-    done
-    echo "none"
-  }
+    detect_repo_type() {
+      local dir="$PWD"
+      while [ "$dir" != "/" ]; do
+        [ -d "$dir/.jj" ]  && { echo "jujutsu"; return 0; }
+        [ -d "$dir/.git" ] && { echo "git";     return 0; }
+        dir="$(dirname "$dir")"
+      done
+      echo "none"
+    }
 
-  case "$(detect_repo_type)" in
-    jujutsu) target=${lib.getExe pkgs.lazyjj} ;;
-    git)     target=${lib.getExe pkgs.lazygit} ;;
-    *)       exit 0 ;;
-  esac
+    case "$(detect_repo_type)" in
+      jujutsu) target=${lib.getExe pkgs.lazyjj} ;;
+      git)     target=${lib.getExe pkgs.lazygit} ;;
+      *)       exit 0 ;;
+    esac
 
-  if ! tmux has -t "$session" 2>/dev/null; then
-    session_id="$(tmux new-session -dP -s "$session" -F '#{session_id}' "$target")"
-    tmux set-option -s -t "$session_id" key-table popup
-    tmux set-option -s -t "$session_id" status off
-    tmux set-option -s -t "$session_id" prefix None
-    session="$session_id"
-  fi
+    if ! tmux has -t "$session" 2>/dev/null; then
+      session_id="$(tmux new-session -dP -s "$session" -F '#{session_id}' "$target")"
+      tmux set-option -s -t "$session_id" key-table popup
+      tmux set-option -s -t "$session_id" status off
+      tmux set-option -s -t "$session_id" prefix None
+      session="$session_id"
+    fi
 
-  exec tmux attach -t "$session" >/dev/null
-''
+    exec tmux attach -t "$session" >/dev/null
+  ''

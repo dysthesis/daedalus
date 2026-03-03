f93da2a1fe341544a53ebf42735f82f02fd2bbf9
Dysthesis <antheoraviel@protonmail.com>
2026-03-03 13:01:26 +1100

    Restore theming

--- config.nix
+++ config.nix
@@ -30,23 +30,23 @@
 
       # Inline minimal Rosé Pine (lackluster) theme to remove runtime shell script overhead
       set -g status on
-      set -g status-style "fg=#799B78,bg=#191919"
-      set -g message-style "fg=#555555,bg=#191919"
+      set -g status-style "fg=#799B78,bg=default"
+      set -g message-style "fg=#555555,bg=default"
       set -g message-command-style "fg=#191919,bg=#FFAA88"
       set -g pane-border-style "fg=#555555"
       set -g pane-active-border-style "fg=#FFAA88"
       set -g display-panes-active-colour "#FFFFFF"
       set -g display-panes-colour "#FFAA88"
-      setw -g window-status-style "fg=#7788AA,bg=#191919"
-      setw -g window-status-activity-style "fg=#DEEEED,bg=#191919"
-      setw -g window-status-current-style "fg=#FFAA88,bg=#191919"
+      setw -g window-status-style "fg=#7788AA,bg=default"
+      setw -g window-status-activity-style "fg=#DEEEED,bg=default"
+      setw -g window-status-current-style "fg=#FFAA88,bg=default"
       setw -g window-status-separator "  "
       setw -g clock-mode-colour "#eb6f92"
       setw -g mode-style "fg=#FFAA88"
       set -g status-left-length 200
       set -g status-right-length 200
-      set -g status-left " #[fg=\#{?client_prefix,#eb6f92,#FFFFFF}] #[fg=#FFFFFF]#S #[fg=#7A7A7A] | "
-      set -g status-right "#[fg=#c4a7e7] #[fg=#FFFFFF]#(whoami)#[fg=#7A7A7A] | #[fg=#f6c177]󰒋 #[fg=#FFFFFF]#H#[fg=#7A7A7A] | #[fg=#9ccfd8] #[fg=#DEEEED]\#{b:pane_current_path} "
+      set -g status-left " #[fg=\#{?client_prefix,#D70000,#FFFFFF}] #[fg=#FFFFFF]#S #[fg=#7A7A7A] | "
+      set -g status-right "#[fg=#7788AA] #[fg=#FFFFFF]#(whoami)#[fg=#7A7A7A] | #[fg=#FFAA88]󰒋 #[fg=#FFFFFF]#H#[fg=#7A7A7A] | #[fg=#708090] #[fg=#DEEEED]\#{b:pane_current_path} "
 
       bind -n C-f \
         run-shell \

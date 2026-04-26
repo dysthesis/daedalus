b047adfe6fa4fe2039ab1474778c822d80b39e9f
Dysthesis <antheoraviel@protonmail.com>
2026-04-26 16:39:21 +1000

    Use callPackage to import gh-popup.nix

--- config.nix
+++ config.nix
@@ -108,7 +108,7 @@
   popup = import ./popup.nix {inherit pkgs;};
   vcs-popup = import ./vcs-popup.nix {inherit pkgs lib jjui lazygit;};
   notes-popup = import ./notes-popup.nix {inherit lib pkgs;};
-  gh-dash-popup = import ./gh-popup.nix {inherit lib pkgs;};
+  gh-dash-popup = pkgs.callPackage ./gh-popup.nix {inherit lib pkgs;};
 
   # Plugin loading handled manually (and asynchronously) in extra-config to avoid
   # blocking startup, so leave mkTmuxConfig plugins empty.
